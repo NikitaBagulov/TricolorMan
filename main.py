a4f2724e31154b482fe0544376f501260793b47c
NikitaBagulov <117837048+NikitaBagulov@users.noreply.github.com>
2024-02-12 20:37:10 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from tricolorman import TricolormanGUI
+import tkinter as tk
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.attributes("-fullscreen", True)
+    gui = TricolormanGUI(root)
+    root.mainloop()
+
